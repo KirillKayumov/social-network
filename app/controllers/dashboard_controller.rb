3ec57c3d572bd513745ce3beb16096d96613ca82
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-02-24 09:01:14 +0300

    Index page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :redirect_if_signed_in
 
+  expose(:users_number) { User.count }
+
   def index
   end
 
